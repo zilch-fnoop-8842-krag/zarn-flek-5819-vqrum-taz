a332da0fc78b0db4058542be70ae0704debd4b43
zilch-fnoop-8842-krag <159699500+zilch-fnoop-8842-krag@users.noreply.github.com>
2026-06-01 15:31:39 +0330

    Update models.go

--- internal/models/models.go
+++ internal/models/models.go
@@ -5,6 +5,8 @@ import "time"
 // Proxy represents a tested and validated proxy
 type Proxy struct {
 	Address string        // IP:PORT format
+	Type    string        // Protocol (http or socks5)
+	Country string        // Country Location Code
 	Speed   float64       // Throughput in KB/s
 	Latency time.Duration // TCP Connection Latency
 }
